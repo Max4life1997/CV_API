d9150b9dfd4ed70dbf2e03a65f91929c88161ce1
Max <htpbltyn57@gmail.com>
2023-12-22 15:27:37 +0300

    Add JenkinsFile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,36 @@
 pipeline {
-    agent any
+    agent {
+        label 'linux'
+    }
 
     stages {
-       stage('JenkinsFile-Build-Steps'){
-           steps{
-               echo "Start"
-               echo "Doing"
-               echo "End"
-           }
-       }
+        stage('JenkinsFile-Build-Steps') {
+            steps {
+                echo "Start"
+                echo "Doing"
+                echo "End"
+            }
+        }
+        stage('MVN Clean') {
+            steps {
+                echo "Производится очистка билда"
+//                container('maven') {
+                    script {
+
+                        sh 'mvn clean compile'
+
+                    }
+//                }
+            }
+        }
+        stage('Tests Launch') {
+            steps {
+                container('maven') {
+                    script {
+
+                    }
+                }
+            }
+        }
     }
 }
